edc6715755775ea1271d4b64158f16dfc48eda93
zicca <ziq@zjut.edu.cn>
2025-08-13 11:38:55 +0800

    feat(starter)：基于Apollo实现线程池参数配置

---
+++ starter/apollo-spring-boot-starter/src/main/java/com/zicca/zthread/apollo/springboot/starter/configuration/ApolloAutoConfiguration.java
@@ -0,0 +1,26 @@
+package com.zicca.zthread.apollo.springboot.starter.configuration;
+
+import com.zicca.zthread.apollo.springboot.starter.refresher.ApolloRefresherHandler;
+import com.zicca.zthread.core.config.BootstrapConfigProperties;
+import com.zicca.zthread.spring.base.enable.MarkerConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Apollo 配置中心自动装配
+ *
+ * @author zicca
+ */
+@AutoConfigureOrder(Integer.MIN_VALUE)
+@ConditionalOnBean(MarkerConfiguration.Marker.class)
+@ConditionalOnProperty(prefix = BootstrapConfigProperties.PREFIX, value = "enable", matchIfMissing = true, havingValue = "true")
+public class ApolloAutoConfiguration {
+
+    @Bean
+    public ApolloRefresherHandler apolloRefresherHandler(BootstrapConfigProperties properties) {
+        return new ApolloRefresherHandler(properties);
+    }
+
+}
